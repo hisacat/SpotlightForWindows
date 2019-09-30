37b76e79daedee788b9d604f979bd49b99177881
HisaCat <ahisacat@gmail.com>
2019-09-30 15:27:06 +0900

    Design viewing test

---
+++ SpotlightForWindows/SpotlightWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SpotlightForWindows
+{
+    /// <summary>
+    /// Spotlight.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SpotlightWindow : Window
+    {
+        public SpotlightWindow()
+        {
+            Init();
+
+            InitializeComponent();
+        }
+
+        private void Init()
+        {
+            DelegateWindowEvents();
+        }
+
+        private void DelegateWindowEvents()
+        {
+            this.Deactivated += SpotlightWindow_Deactivated;
+        }
+
+        private void SpotlightWindow_Deactivated(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
